804f0592fea0a5ddd5a5609eb63875ffa07a616b
juliams1 <julia_maiasantos@hotmail.com>
2023-11-30 17:43:36 -0300

    Criação da Classe IceCream Factory add primeiro método makeIceCream

---
+++ IceCreamFactory.java
@@ -0,0 +1,10 @@
+/**
+ * Ice-Cream-Factory
+ */
+public class IceCreamFactory {
+
+    public public void makeIceCream(String[] args) {
+        System.out.println("Produzimos sorvetes sem moderação");
+    }
+}
+
